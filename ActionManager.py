9782cf1d6e200e953d2a7afb9a641a57d61f06ed
Benta63 <noahstolz@yahoo.com>
2018-09-25 20:07:26 -0400

    Added some to Action Manager

--- ActionManager.py
+++ ActionManager.py
@@ -1,16 +1,51 @@
+import sys
 import StategoBoard
 
 class PlaceManager(object):
 	def __init__():
 		self.board = [10]
-	def setBoard(self):
-		#Get the coordinates of the lakes
-
+	
+	#An error function for debugging
 	def ErrorFunction(self, type):
-		print "Something Something Error Error"
-		
+		print("Error: {:s}".format(type))
+		sys.exit()
+	
+	#For initial placing of troops on the board	
 	def placeTroop(StrategoBoard, troop, x,y):
 		#Can only place in rows 0-3
-		if y > 3:
+		if x == 4 or x == 5:
 			self.ErrorFunction("Placing")
 
+	#Returns True if the attacker wins and False if the defender wins. Also returns false if the defender is equal to the attacker
+	def Attack(Attacker, Defender):
+		#Edge cases of Spy, bomb or flag: 
+		if Defender == 'S':
+			return True
+		elif Defender == 'B':
+			return False
+		elif Defender == 'F':
+			return True #And Attacker wins the game
+		else:
+			return (int(Attacker) >= int(Defender))
+			
+
+	def updateBoard(xstart, ystart, xend, yend):
+		if not islegal(xend, yend): self.ErrorFunction("Moving")
+		#MAKE islegal function
+		
+		if StrategoBoard.MapData[xend, yend] != ['','']:
+			#The location is empty, so we place the piece
+			StrategoBoard.MapData[xend,yend] == [color,StrategoBoard.Piece(xstart,ystart)]
+		else:
+			#It's an attack
+			if StrategoBoard.Piece(xend,yend) == 'F':
+				print("You won!!!") 
+				#Exit
+			elif self.Attack(StrategoBoard.Piece(xstart,ystart), StrategoBoard.Piece(ystart,yend)):
+				#Won the attack
+				StrategoBoard.MapData[xend,yend] == [StrategoBoard.Color(xstart,ystart),StrategoBoard.Piece(xstart,ystart), 'M']
+			else:
+				#Lost the attack, but we know the piece
+				StrategoBoard.MapData[xend,yend] == [StrategoBoard.Color(xstart,ystart),StrategoBoard.Piece(xstart,ystart),'K']
+		#Whether the piece moving lives or dies, the place it came from will still be empty
+		StrategoBoard.MapData([xstart,ystart]) = ['','']
--- StrategoBoard.py
+++ StrategoBoard.py
@@ -1,25 +1,50 @@
 class StrategoBoard():
 	def __init__(self):
-		self.MapData = []
+		self.MapData = [10]
 		#Going to be tuples, first color, second piece
+	def setBoard(self):
+		#Lakes are [4, 2] [4,3] [4,6] [4,7]
+		#		   [5,2] [5,3] [5,6] [5,7] [row, column]
+		#For each board space, it will be (color, piece, known). (L,L) in the case of lakes.
+		#S = Spy, 1-10 (strings) = Numbers, B = Bomb, F = Flag
+		#Get the coordinates of the lakes
+		#First we initialize an empty board
+		for i in range(0, 10):
+			for j in range(0,10):
+				if i == 4:
+					if j == 2 or j == 3 or j == 6 or j == 7:
+						self.MapData[i].append('L','L')
+				elif i == 5:
+					if j == 2 or j == 3 or j == 6 or j ==7:
+						self.MapData[i].append('L','L')
+				else:
+					self.MapData[i].append(['','',''])
 
 	#Gets where your army is on the map
 	def getArmy(self):
 		mine  = []
 		for i in range(0, len(self.MapData)):
 			for j in range (0, len(self.MapData[i])):
-				if self.MapData[i][j][0] == 'mine':
+				if self.MapData[i][j][0] == 'M':
 					mine.append([i,j])
 		#Returns a list of locations for your pieces
 		return mine
 
-	def whichPiece(self, x, y):
-		return self.MapData[x][y]
+	def Piece(self, x, y):
+		return self.MapData[x][y][1]
+
+	def Color(self, x, y):
+		return self.MapData[x][y][0]
+
+	def isKnown(self,x,y):
+		if self.MapData[i][j][2] == 'K': return True
+		return False
 
 	def knownEnemy(self):
 		theirs = []
 		for i in range(0, len(self.MapData)):
 			for j in range(0, len(self.MapData[i])):
-				if self.MapData[i][j][0] == 'known':
+				if isKnown[i][j]: #K is for known
 					theirs.append(i,j)
 		return theirs
+		
